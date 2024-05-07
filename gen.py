f580901f40cf14a95998b782eb73e666e09ce4c7
vudembeke <vudembeke03@gmail.com>
2024-05-07 16:12:18 +0300

    gen identifier

---
+++ gen.py
@@ -0,0 +1,22 @@
+print("GENERATION IDENTIFIER")
+print()
+year = int(input("Which year were you born? "))
+if year >= 1883 and year <= 1900:
+  print("You are a Lost Generation")
+elif year >=1901 and year <= 1927:
+    print("You are the Greatest Generation") 
+elif year >=1928 and year <= 1945:
+    print("You are the silent generation")
+elif year >=1946 and year <= 1964:
+     print("You are a Baby Boomer")
+elif year >=1965 and year <= 1980:
+    print("You are Generation X")
+elif year >=1981 and year <= 1996:
+    print("You are a Millenial")
+elif year >=1997 and year <= 2012:
+    print("You are Generation Z")
+elif year >=2013:
+    print("You are Generation Alpha")
+  
+   
+  
